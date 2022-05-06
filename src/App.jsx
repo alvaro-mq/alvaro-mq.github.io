0b9f22a6fcf2b77e7ca20d44a29e40302c59045b
AlvaroMQ <alvaro.mamani.qp@gmail.com>
2022-05-05 23:17:55 -0400

    build: add i18n configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import './App.css';
+import { useTranslation } from "react-i18next";
 
 function App() {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguageHandler = (e) => {
+    const languageValue = e.target.value;
+    i18n.changeLanguage(languageValue);
+  }
+
   return (
     <div className="App">
+      <select onChange={changeLanguageHandler}>
+        <option value="en" >English</option>
+        <option value="es" >Spanish</option>
+      </select>
       <div className='text-3xl font-bold underline'>
-        My portfolio
+        {t('text')}
       </div>
     </div>
   );
