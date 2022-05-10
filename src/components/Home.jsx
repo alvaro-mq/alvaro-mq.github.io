0d9db56e3b1d3fdba3780ff0ffc5e3cc07b6a7ed
AlvaroMQ <alvaro.mamani.qp@gmail.com>
2022-05-10 11:20:51 -0400

    feat: custom text sizes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -20,11 +20,11 @@ const Home = () => {
       <div className="flex flex-col justify-center items-center w-full h-full">
         <div className="grid sm:grid-cols-3 gap-8 px-4">
           <div className="sm:col-span-2 max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-            <p className="text-generic text-2xl">{t('home.greetings')}</p>
-            <h1 className="text-7xl font-bold text-light-text-primary dark:text-dark-text-primary">
+            <p className="text-generic text-sm lg:text-2xl">{t('home.greetings')}</p>
+            <h1 className="lg:text-7xl text-3xl font-bold text-light-text-primary dark:text-dark-text-primary">
               Alvaro Mamani
             </h1>
-            <h2 className="text-4xl font-bold text-light-text-accent dark:text-dark-text-accent">
+            <h2 className="text-2xl lg:text-3xl font-bold text-light-text-accent dark:text-dark-text-accent">
               {t('home.iAm')}
             </h2>
             <p className="text-light-text-secondary dark:text-dark-text-secondary py-4 max-w-[700px]">
